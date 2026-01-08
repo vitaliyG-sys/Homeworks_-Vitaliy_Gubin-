3fc3c20d489cf9fa4dca0ee35f2e89c26897edf4
VitaliyGubin <trofim-83@mail.ru>
2026-01-08 23:02:43 +0800

    init homework 9.1

---
+++ src/masks.py
@@ -0,0 +1,30 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Функция принимает номер карты, проверяет правильность введенных данных
+    и возвращает в формате 'XXXX XX** **** XXXX'.
+    Проверяет правильность введенных данных с выводом ошибки.
+    """
+
+    # Удаляем пробелы из полученных данных
+    cleared_card_number = card_number.replace(" ", "")
+
+    # Проверяем правильность введенных данных с выводом ошибки
+    if len(cleared_card_number) != 16 or not cleared_card_number.isdigit():
+        raise ValueError("Invalid card number")
+    return f"{cleared_card_number[:4]} {cleared_card_number[4:6]}** **** {cleared_card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """
+    Функция принимает номер аккаунта, проверяет правильность введенных данных
+    и возвращает в формате '**XXXX'.
+    Проверяет правильность введенных данных с выводом ошибки.
+    """
+
+    # Удаляем пробелы из полученных данных
+    cleared_account_number = account_number.replace(" ", "")
+
+    # Проверяем правильность введенных данных с выводом ошибки
+    if len(cleared_account_number) != 6 or not account_number.isdigit():
+        raise ValueError("Invalid account number")
+    return f"**{cleared_account_number[-4:]}"
