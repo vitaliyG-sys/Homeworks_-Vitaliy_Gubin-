74919f62bbf29aefa33887b7141f9dab1ab61623
VitaliyGubin <trofim-83@mail.ru>
2026-01-09 15:15:02 +0800

    homework 9.2: lj,добавлена функция get_date в модуль wiget.py, e, убраны тестовые функции.

--- src/wiget.py
+++ src/wiget.py
@@ -1,28 +1,35 @@
-from masks import get_mask_card_number, get_mask_account
+from datetime import datetime as dt
 
-def mask_account_card(account_or_card_number : str) -> str:
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(account_or_card_number: str) -> str:
     """
     Функция принимает название и номер счета или карты, вызывает функцию "get_mask_card_number" или "get_mask_account"
     в зависимости от типа данных и возвращает название с замаскированным номером
     """
+
     # Разделяем номер и название объекта.
-    object_number = "".join(x for x in account_or_card_number if x.isdigit())
-    object_type = "".join(x for x in account_or_card_number if not x.isdigit())
+    object_number = "".join(c for c in account_or_card_number if c.isdigit())
+    object_type = "".join(c for c in account_or_card_number if not c.isdigit())
 
     # Вызываем функцию маскировки номера счета/карты, возвращаем отформатированный результат.
     if len(object_number) == 16:
         masked = get_mask_card_number(object_number)
-        return object_type + masked
+        return f"{object_type + masked}"
     elif len(object_number) == 20:
         masked = get_mask_account(object_number)
-        return object_type + masked
+        return f"{object_type + masked}"
 
     return "Invalid account number"
 
 
-#Тестовые данные
-test = ["Maestro 1596837868705199", "Счет 64686473678894779589", "MasterCard 7158300734726758",
-        "Счет 35383033474447895560", "Visa Classic 6831982476737658", "Visa Platinum 8990922113665229",
-        "Visa Gold 5999414228426353", "Счет 73654108430135874305"]
-for x in test:
-    print(mask_account_card(x))
+def get_date(iso_date: str) -> str:
+    """
+    Функция которая принимает строку с датой формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ"
+    """
+    # Форматируем строку в datetime
+    date_time = dt.strptime(iso_date, "%Y-%m-%dT%H:%M:%S.%f")
+    # Возвращаем строку в нужном формате
+    return date_time.strftime("%d.%m.%Y")
