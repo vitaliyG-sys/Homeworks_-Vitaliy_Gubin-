8dbe51f793deca288d8e61f080ad8912c3449c0b
VitaliyGubin <trofim-83@mail.ru>
2026-01-09 15:11:01 +0800

    homework 9.2: переделан masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,30 +1,12 @@
 def get_mask_card_number(card_number: str) -> str:
     """
-    Функция принимает номер карты, проверяет правильность введенных данных
-    и возвращает в формате 'XXXX XX** **** XXXX'.
-    Проверяет правильность введенных данных с выводом ошибки.
+    Функция принимает номер карты и возвращает в формате 'XXXX XX** **** XXXX'.
     """
-
-    # Удаляем пробелы из полученных данных
-    cleared_card_number = card_number.replace(" ", "")
-
-    # Проверяем правильность введенных данных с выводом ошибки
-    if len(cleared_card_number) != 16 or not cleared_card_number.isdigit():
-        raise ValueError("Invalid card number")
-    return f"{cleared_card_number[:4]} {cleared_card_number[4:6]}** **** {cleared_card_number[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """
-    Функция принимает номер аккаунта, проверяет правильность введенных данных
-    и возвращает в формате '**XXXX'.
-    Проверяет правильность введенных данных с выводом ошибки.
+    Функция принимает номер аккаунта и возвращает в формате '**XXXX'.
     """
-
-    # Удаляем пробелы из полученных данных
-    cleared_account_number = account_number.replace(" ", "")
-
-    # Проверяем правильность введенных данных с выводом ошибки
-    if len(cleared_account_number) != 20 or not account_number.isdigit():
-        raise ValueError("Invalid account number")
-    return f"**{cleared_account_number[-4:]}"
+    return f"**{account_number[-4:]}"
