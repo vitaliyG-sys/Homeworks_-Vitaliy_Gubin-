ab30db30f442a9eeae5c7b358d752461e7c5ec60
VitaliyGubin <trofim-83@mail.ru>
2026-01-16 00:05:57 +0800

    в processing.py добавлена функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,15 @@
 def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """ Функция принимает список словарей и возвращает
+    """Функция принимает список словарей и возвращает
     список словарей со значением ключа state:"EXECUTED" """
     executed_operations = []
     for operation in operations:
         if operation.get("state") == "EXECUTED":
             executed_operations.append(operation)
             continue
+    return executed_operations
+
+
+def sort_by_date(operations: list[dict]) -> list[dict]:
+    """Функция принимает список словарей и возвращает
+    список словарей отсортированный по дате в порядке убывания"""
+    return sorted(operations, key=lambda operation: operation["date"], reverse=True)
