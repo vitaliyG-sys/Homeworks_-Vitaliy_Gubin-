e38d94943f88ef60703a88d86584af99529356c2
VitaliyGubin <trofim-83@mail.ru>
2026-04-08 11:41:37 +0800

    добавлен файл read_files.py

---
+++ src/read_files.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def read_csv(path: str) -> list[dict] | OSError:
+    """Функция для считывания финансовых операций из CSV. Возвращает список словарей с транзакциями."""
+    try:
+        with open(path, 'r', newline='', encoding="utf-8") as csv_file:
+            df = pd.read_csv(csv_file,delimiter=";")
+        result = df.to_dict(orient="records")
+        return result
+    except FileNotFoundError as error:
+        return error
+
+
+def read_excel(path: str) -> list[dict] | OSError:
+    """Функция для считывания финансовых операций из Excel. Возвращает список словарей с транзакциями."""
+    try:
+        df = pd.read_excel(path)
+        result = df.to_dict(orient="records")
+        return result
+    except FileNotFoundError as error:
+        return error
